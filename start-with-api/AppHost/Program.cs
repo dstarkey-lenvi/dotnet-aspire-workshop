7632709d5ab0cff59d59227228061588a49abf92
David Starkey <david.starkey@lenvi.com>
2025-02-12 14:40:47 +0000

    updates

--- start-with-api/AppHost/Program.cs
+++ start-with-api/AppHost/Program.cs
@@ -1,9 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 var myCache = builder.AddRedis("cache")
-    .WithImageRegistry("ghcr.io")
-    .WithImage("microsoft/garnet")
-    .WithImageTag("latest");
+    .WithRedisCommander();
+
+    //.WithImageRegistry("ghcr.io")
+    //.WithImage("microsoft/garnet")
+    //.WithImageTag("latest");
 
 var myApi = builder.AddProject<Projects.Api>("api")
     .WithReference(myCache);
